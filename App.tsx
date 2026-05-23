31a0a0b274808cc1492d0c50ac7756647c1f7f25
ashishmisal23 <ashishmisal23@gmail.com>
2026-05-24 03:29:11 +0530

    updated files for the push notification

--- App.tsx
+++ App.tsx
@@ -5,8 +5,21 @@
  * @format
  */
 
+import React, { useEffect } from 'react';
+
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+import {
+  PermissionsAndroid,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import messaging from '@react-native-firebase/messaging';
+
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -14,6 +27,60 @@ import {
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    requestNotificationPermission();
+
+    getFcmToken();
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('Foreground Notification:', remoteMessage);
+    });
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification opened from background state:',
+        remoteMessage,
+      );
+    });
+
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification opened from killed state:',
+            remoteMessage,
+          );
+        }
+      });
+
+    return unsubscribe;
+  }, []);
+
+  async function requestNotificationPermission() {
+    if (Platform.OS === 'android' && Platform.Version >= 33) {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        );
+
+        console.log('Notification Permission:', granted);
+      } catch (error) {
+        console.log('Permission Error:', error);
+      }
+    }
+  }
+
+  async function getFcmToken() {
+    try {
+      const token = await messaging().getToken();
+
+      console.log('FCM TOKEN:', token);
+    } catch (error) {
+      console.log('FCM TOKEN ERROR:', error);
+    }
+  }
 
   return (
     <SafeAreaProvider>
